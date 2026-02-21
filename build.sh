7b717dca8eee3febbb38ea9cb53dccc5acdb0eff
NaveenPAnil <naveenpanil2000@gmail.com>
2026-02-21 10:18:38 +0530

    render updation superuser

--- build.sh
+++ build.sh
@@ -8,5 +8,8 @@ pip install -r requirements.txt
 # Run migrations
 python manage.py migrate
 
+# Create superuser (if env vars provided)
+python manage.py shell < create_superuser.py
+
 # Collect static files
 python manage.py collectstatic --no-input
