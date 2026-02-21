9c0bb12e925578ce8f8cabe52b5cca508b31ea59
NaveenPAnil <naveenpanil2000@gmail.com>
2026-02-21 10:37:48 +0530

    superuser create

--- create_superuser.py
+++ create_superuser.py
@@ -2,15 +2,24 @@ import os
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+
 email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
 password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+first_name = os.environ.get('DJANGO_SUPERUSER_FIRST_NAME', 'Admin')
+last_name = os.environ.get('DJANGO_SUPERUSER_LAST_NAME', 'User')
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
 
-if username and password:
-    if not User.objects.filter(username=username).exists():
-        print(f"Creating superuser: {username}")
-        User.objects.create_superuser(username=username, email=email, password=password)
+if email and password:
+    if not User.objects.filter(email=email).exists():
+        print(f"Creating superuser: {email}")
+        User.objects.create_superuser(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            email=email,
+            password=password
+        )
     else:
-        print(f"Superuser {username} already exists.")
+        print(f"Superuser {email} already exists.")
 else:
-    print("Superuser credentials not found in environment variables. Skipping.")
+    print("Superuser credentials not found. Skipping.")
