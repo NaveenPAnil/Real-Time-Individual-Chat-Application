cb76db0a2d021142100ffecf9685a5ab22d020f0
NaveenPAnil <naveenpanil2000@gmail.com>
2026-09-18 13:29:32 +0530

    setup ci cd pipeline

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class AccountsAccessibilityTest(TestCase):
+    def test_login_page_loads(self):
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/login.html')
+
+    def test_register_page_loads(self):
+        response = self.client.get(reverse('registerUser'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/register.html')
--- chatapp/settings.py
+++ chatapp/settings.py
@@ -114,8 +114,12 @@ AUTH_USER_MODEL = 'accounts.User'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('Db_name'),
+        'USER': config('Db_user'),
+        'PASSWORD': config('Db_password'),
+        'HOST': config('Db_host'),
+        'PORT': config('Db_port'),
     }
 }
 
